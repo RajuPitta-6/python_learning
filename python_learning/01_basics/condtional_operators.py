a4f9dd0c8bcff5186cecb3306b52728f7c49224c
Raju Pitta <appalarajupitta9@gmail.com>
2025-08-07 08:32:41 +0530

    Added basics and conditional statements

---
+++ python_learning/01_basics/condtional_operators.py
@@ -0,0 +1,15 @@
+a = 5
+b = 6
+'''
+==  equal to 
+<  less than 
+> greater than
+!= not equal to
+>= greater than or equal to
+<= less than or equal to'''
+print(a == b)
+print(a < b)
+print(a > b)
+print(a != b)
+print(a >= b)
+print(a <= b)
--- python_learning/01_basics/type_casting.py
+++ python_learning/01_basics/type_casting.py
@@ -4,7 +4,7 @@ b = 36.06 # float
 
 a = float(a) # int to float
 b = int(b) # float to int
-c = str(a) # int to str
+c = str(a) # int to str+
 print("a =", a,    type(a))
 print("b =" , b,type(b))
 print("c =", c,type(c))
