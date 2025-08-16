b11f2815e0fd626258ed6d2dada00143042f42b8
Raju Pitta <appalarajupitta9@gmail.com>
2025-08-16 16:11:01 +0530

    Updating funtions learning as part of Day-9

---
+++ python_learning/functions/syntax_basic_function.py
@@ -0,0 +1,9 @@
+# def fun(parameter):
+#     return (expression) #Body of the funtion
+# fun(n) # function call
+
+def greet():
+    print("Welcome to my future world")
+    print("are you ready......")
+
+greet()
