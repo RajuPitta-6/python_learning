f45dd6e1115a3a2958afdf794b4c9af1af138724
Raju Pitta <appalarajupitta9@gmail.com>
2025-09-12 21:45:39 +0530

    Added file_handling(write_file) learning

---
+++ python_learning/File_Handlings/write_file.py
@@ -0,0 +1,14 @@
+# writing student details into a file
+
+with open("students.txt", "w") as file:
+    file.write("Name: Razu\n")
+    file.write("Branch: cse\n")
+    file.write("College: RK\n\n")
+
+    file.write("Name: swathi\n")
+    file.write("Branch: physics\n")
+    file.write("College: C4\n\n")
+
+    file.write("Name: Jr\n")
+    file.write("Branch: Teliyadhu\n")
+    file.write("College: RK\n\n")
